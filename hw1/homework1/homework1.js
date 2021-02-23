1add790216d61f9bcdaf0af4ecd051b482ada402
Max <46355617+Makcumka5@users.noreply.github.com>
2021-02-23 16:42:19 +0300

    homework1

---
+++ hw1/homework1/homework1.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//1
+const Tc = 40;
+const Tf = (9 / 5) * Tc + 32;
+console.log(Tf);
+//2
+let admin;
+let name;
+name = "Василий";
+admin = name;
+console.log(admin);
+//3 1000108
+console.log(1000 + "108");
+//4
+//async & defer разобраны)
